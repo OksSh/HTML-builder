2a25c81d296c9d28cb1c0dcdf951328efaca9381
Oksana Shkulepa <o.shkulepa@gmail.com>
2023-05-09 02:47:10 +0300

    feat: add 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const { error } = require('console');
+const fs = require('fs');
+const path = require('path');
+const { stdin } = process;
+
+fs.writeFile(path.join(__dirname, 'text.txt'), '', (error) => {
+  if (error) throw error;
+  console.log('Enter text..');
+});
+
+stdin.on('data', (data) => {
+  fs.appendFile(path.join(__dirname, 'text.txt'), data.toString(), (error) => {
+    if (error) throw error;
+    if (data.toString().trim().toLowerCase() === 'exit') {
+      process.exit();
+    }
+  });
+});
+
+process.on('exit', () => {
+  console.log('Good bye!');
+});
